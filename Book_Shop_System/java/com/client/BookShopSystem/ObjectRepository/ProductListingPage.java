f7f6476d2b9dcf39a5151e4cb3449238d2622fb2
khankaif4877-ux <khankaif4877@gmail.com>
2025-10-16 13:19:15 +0530

    " new commit "

--- Book_Shop_System/java/com/client/BookShopSystem/ObjectRepository/ProductListingPage.java
+++ Book_Shop_System/java/com/client/BookShopSystem/ObjectRepository/ProductListingPage.java
@@ -11,37 +11,41 @@ public class ProductListingPage {
 	WebDriverUtility webDrUtil;
 
 	public ProductListingPage(WebDriver driver) {
-	this.driver = driver;
-	webDrUtil = new WebDriverUtility(driver);
-	PageFactory.initElements(driver, this);
+		this.driver = driver;
+		webDrUtil = new WebDriverUtility(driver);
+		PageFactory.initElements(driver, this);
 
 	}
-	
+
 	@FindBy(xpath = "//div[@class='book-block' and contains(., 'Like a Love Song')]")
 	private WebElement randomeBook;
+
 	public WebElement getRandomeBook() {
 		return randomeBook;
 	}
-	
+
 	@FindBy(xpath = "//div[@class='book-block']")
 	private WebElement firstBook;
+
 	public WebElement getFirstBook() {
 		return firstBook;
 	}
+
 	@FindBy(xpath = "//div[@id='heading']/h2")
 	private WebElement heading;
+
 	public WebElement getHeading() {
 		return heading;
 	}
+
 	@FindBy(xpath = "//span[contains(@style,'line-through;')]")
 	private WebElement actualPrice;
 	public WebElement getActualPrice() {
 		return actualPrice;
 	}
-	
-	public int getPriceAfterDiscount(WebElement element ) {
-		
 
-	return Integer.parseInt( element.getText().substring(element.getText().length() - 13,element.getText().length() - 10));
-}
+	public int getPriceAfterDiscount(WebElement element) {
+		String number =  element.getText().substring(element.getText().length() - 13,element.getText().length() - 10).replaceAll("[^0-9.]", "");
+		return Integer.parseInt(number);
+	}
 }
--- Book_Shop_System/src/test/java/com/client/BookShop_KK/Module/ChildrenAndTeens/ChildrenAndTeensTest.java
+++ Book_Shop_System/src/test/java/com/client/BookShop_KK/Module/ChildrenAndTeens/ChildrenAndTeensTest.java
@@ -22,6 +22,19 @@ public class ChildrenAndTeensTest extends BaseClass {
     	Assert.assertTrue(priceAfterDiscount>0);
 		plp.getActualPrice().isDisplayed();
 	}
+	@Test(groups = "integration")
+	public void DemochildAndTeensLinkAndProductListingPageTest() {
+		 String value = exlutil.getDataFromExcelSheet("Kaif Khan", 5, 0);
+		hp.getSideBarText(value).click();
+//		hp.getchildAndTeenLink().click();
+//		Assert.assertEquals(text.toLowerCase(), n.toLowerCase());
+		Assert.assertEquals(plp.getHeading().isDisplayed(), true);
+		plp.getFirstBook().isDisplayed();
+		WebElement book = plp.getFirstBook();
+		int priceAfterDiscount =plp.getPriceAfterDiscount(book);
+    	Assert.assertTrue(priceAfterDiscount>0);
+		plp.getActualPrice().isDisplayed();
+	}
 
 	@DataProvider(name = "categoryNamesData")
 	public Object[][] categoryNamesData() {
